dce6d53e21c874e0a8ef3c7a3f4d5347c9650a31
tsyu12345 <syukut@gmail.com>
2023-08-22 10:43:56 +0900

    【拝啓基盤制作】グリッドシステムでレスポンシブな基盤背景を作成。

--- FrontEnd/ChibaUniversityFutsal/src/App.vue
+++ FrontEnd/ChibaUniversityFutsal/src/App.vue
@@ -1,85 +1,62 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-
-      </nav>
+  <header class="TopHeader fixed-top">
+    <div class="row">
+      <div class="col">
+        <!--追加のボタン配置用-->
+      </div>
+      <div class="col LogoWrapper">
+        <img src="@/assets/logo.png" alt="ロゴ" class="Logo">
+      </div>
+      <div class="col">
+        <!--追加のボタン配置用-->
+      </div>
     </div>
   </header>
+  <div class="ContentArea container">
+    <!--以下にコンテンツを配置-->
+  </div>
+  <footer class="Footer fixed-bottom shadow-lg">
+    <div class="row" >
+      <div class="col">
+      </div>
+      <div class="col TopButtonWrapper">
+        <!--トップボタン用-->
+      </div>
+    </div>
+    <div class="col"></div>
+  </footer>
 
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.TopHeader {
+  background-color: #f7C223;
   width: 100%;
-  font-size: 12px;
+}
+
+.LogoWrapper {
   text-align: center;
-  margin-top: 2rem;
+  padding: 9px;
+}
+.Logo {
+  width: 30%;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.ContentArea {
+  overflow: auto;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.Footer{
+  background-color: #f7C223;
+  height: 7%;
+  width: 100%;
 }
 </style>
