1317bd74a5fb6b90c262e43b2154308c9f7b3724
miyazawa11 <k.miyazawa.howcollect@gmail.com>
2023-08-22 09:42:32 +0900

    bottonコンポーネントとtitleコンポーネント

---
+++ FrontEnd/ChibaUniversityFutsal/src/components/BaseTitle.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    title:String
+})
+</script>
+    
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1{
+    font-weight: bold;
+    color: rgb(80, 80, 80);
+    text-align: center;
+    margin: 30px 0 30px 0;
+}
+</style>
