b692d407c4ba705a2129da37059ae61f493a1f35
kenc713 <kenc71388@gmail.com>
2023-08-23 16:31:28 +0900

    フォーム用ラベル、変数名等の修正

--- BackEnd/attendance/views/teacher.py
+++ BackEnd/attendance/views/teacher.py
@@ -16,39 +16,39 @@ def teachers():
 @teacher_bp.route('/reserve', methods=['GET', 'POST', 'PUT'])
 def reserve():
     if request.method == 'GET':
-        child_id = request.args.get('child', type=int)
+        id_children = request.args.get('id_childlen', type=int)
         date = request.args.get('date')
 
         # 園児のIDと日付がどちらも指定されていない場合はエラーレスポンスを返す
-        if child_id is None and date is None:
-            return "Error: You should specify either child_id or date", 400
+        if id_children is None and date is None:
+            return "Error: You should specify either id_children or date", 400
 
         # 園児のIDしか指定されていない時
         elif date is None:
-            attendances = attendance.Attendance.query.filter_by(id_children=child_id).all()
-            attendance_schema = attendance.Attendance_schema(many=True).dump(attendances)
+            attendances = attendance.Attendance.query.filter_by(id_children=id_children).all()
+            attendance_schema = attendance.AttendanceSchema(many=True).dump(attendances)
         
         # 日付しか指定されていない時
-        elif child_id is None:
+        elif id_children is None:
             date_split = date.split('_')
             date_split = [int(date) for date in date_split]
             day = datetime.date(year=date_split[0], month=date_split[1], day=date_split[2])
             attendances = attendance.Attendance.query.filter_by(date=day).all()
-            attendance_schema = attendance.Attendance_schema(many=True).dump(attendances)
+            attendance_schema = attendance.AttendanceSchema(many=True).dump(attendances)
 
         else:
             date_split = date.split('_')
             date_split = [int(date) for date in date_split]
             day = datetime.date(year=date_split[0], month=date_split[1], day=date_split[2])
-            attendances = attendance.Attendance.query.filter_by(id_children=child_id, date=day).all()
-            attendance_schema = attendance.Attendance_schema(many=True).dump(attendances)
+            attendances = attendance.Attendance.query.filter_by(id_children=id_children, date=day).all()
+            attendance_schema = attendance.AttendanceSchema(many=True).dump(attendances)
 
         return attendance_schema
     
     if request.method == 'POST':
         payload = request.form
 
-        child_id = payload['id']
+        id_children = payload['id_children']
         submitted_presence = True if payload['submitted_presence'] == 1 else False
         was_present = True if payload['was_present'] == 1 else False
         reason = payload['reason']
@@ -61,12 +61,12 @@ def reserve():
         day = datetime.date(year=date_split[0], month=date_split[1], day=date_split[2])
 
         # すでにその日の出欠が登録されている場合はエラーレスポンスを返す
-        existing_attendances = attendance.Attendance.query.filter_by(id_children=child_id, date=day).all()
+        existing_attendances = attendance.Attendance.query.filter_by(id_children=id_children, date=day).all()
         if existing_attendances:
             return "Error: This attendance has already reserved", 400
 
         # 新しい出欠を登録する
-        new_attendance = attendance.Attendance(id_children=child_id, submitted_presence=submitted_presence, was_present=was_present, reason=reason, is_accepted=is_accepted, reply_to_reason=reply_to_reason, date=day)
+        new_attendance = attendance.Attendance(id_children=id_children, submitted_presence=submitted_presence, was_present=was_present, reason=reason, is_accepted=is_accepted, reply_to_reason=reply_to_reason, date=day)
 
         db.session.add(new_attendance)
         db.session.commit()
@@ -74,17 +74,17 @@ def reserve():
         return "OK", 200
 
     if request.method == 'PUT':
-        child_id = request.args.get('child', type=int)
+        id_children = request.args.get('id_children', type=int)
         date = request.args.get('date')
 
         # 園児のIDと日付がどちらも指定されていない場合はエラーレスポンスを返す
-        if child_id is None or date is None:
-            return "Error: You should specify both child_id and date", 400
+        if id_children is None or date is None:
+            return "Error: You should specify both id_children and date", 400
 
         date_split = date.split('_')
         date_split = [int(date) for date in date_split]
         day = datetime.date(year=date_split[0], month=date_split[1], day=date_split[2])
-        att = attendance.Attendance.query.filter_by(id_children=child_id, date=day).one()
+        att = attendance.Attendance.query.filter_by(id_children=id_children, date=day).one()
 
         # その日の出欠が登録されていない場合はエラーレスポンスを返す
         if att is None:
