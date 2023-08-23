5c30fda39b402844b7fd5d8fe4ea5bd68c83157f
tsyu12345 <syukut@gmail.com>
2023-08-23 14:02:52 +0900

    PC向けレイアウト調整。スマホレイアウトとPC用レイアウトでTemplateを分けた。
    多分もっといいやりかたあるが、時間かかりうなんでこれで。

--- FrontEnd/ChibaUniversityFutsal/src/App.vue
+++ FrontEnd/ChibaUniversityFutsal/src/App.vue
@@ -7,7 +7,7 @@ import BaseIconButton from "@/components/BaseIconButton.vue";
 </script>
 
 <template>
-  <header class="TopHeader fixed-top">
+  <header class="TopHeader BaseColor fixed-top vh-9 vh-sm-10">
     <div class="row">
       <div class="col">
         <!--追加のボタン配置用-->
@@ -20,16 +20,15 @@ import BaseIconButton from "@/components/BaseIconButton.vue";
       </div>
     </div>
   </header>
-  <div class="d-flex align-items-center justify-content-center min-vh-100">
+  <div class="ContentWrapper d-flex align-items-center py-sm-21 pt-sm-25 px-sm-0 overflow-hidden">
     <!--以下にコンテンツを配置-->
-    <div class="">
+    <div class="ContentParent">
       <router-link to="/"></router-link>
       <router-view></router-view>
     </div>
-    <BaseCalender></BaseCalender>
   </div>
-  <footer class="Footer fixed-bottom shadow-lg">
-    <div class="row" >
+  <footer class="Footer fixed-bottom shadow-lg border-top border-3">
+    <div class="row d-sm-none" >
       <div class="col">
       </div>
       <div class="col">
@@ -43,14 +42,25 @@ import BaseIconButton from "@/components/BaseIconButton.vue";
       </div>
       <div class="col"></div>
     </div>
-    
+    <div class="row d-none d-md-block">
+      <div class="col-3">
+        <div class="IconButtonWrapper position-relative mx-auto">
+          <BaseIconButton name="出欠" class="IconButtonSize">
+            <template v-slot:BaseIconButton>
+              <i class="fa-solid fa-user fa-2xs"></i>
+            </template>
+          </BaseIconButton>
+        </div>
+      </div>
+      <div class="col-3"></div>
+      <div class="col-3"></div>
+    </div>
   </footer>
 
 </template>
 
 <style scoped>
 .TopHeader {
-  background-color: #f7C223;
   width: 100%;
 }
 
@@ -62,18 +72,21 @@ import BaseIconButton from "@/components/BaseIconButton.vue";
   width: 30%;
 }
 
-.ContentArea {
-  overflow: auto;
-  margin-top: 35%;
-  height: 100%;
+.ContentWrapper {
+  background-color: #ffffff;
+  width: 100%;
+  height: 100vh;
+  padding-top: 13%;
 }
 
-.ContentArea > .row {
-  margin-bottom: 10%;
+.ContentParent {
+  width: 100%;
+  height: auto;
+  max-height: 100%;
 }
 
 .Footer{
-  background-color: #f7C223;
+  background-color: #ffffff;
   height: 7%;
   width: 100%;
 }
@@ -81,6 +94,48 @@ import BaseIconButton from "@/components/BaseIconButton.vue";
 .IconButtonWrapper {
   top: -33%;
   width: fit-content;
+}
 
+.IconButtonSize{
+  transform: scale(2.5);
+  position: relative;
+  top: -3vw;
+}
+
+.min-vh-90 {
+  min-height: 70vh !important;
+}
+
+.py-21 {
+  padding-top: 21%;
+  padding-bottom: 21%;
+}
+
+.pt-21 {
+  padding-top: 21%;
+}
+
+/* スマホのみに適用 */
+@media (max-width: 767.98px) {
+  .vh-sm-10 {
+      height: 10vh !important;
+  }
+
+  .px-sm-0 {
+      padding-right: 0 !important;
+      padding-left: 0 !important;
+  }
+
+  .py-sm-21 {
+      padding-top: 21% !important;
+      padding-bottom: 21% !important;
+  }
+
+  .pt-sm-25 {
+    padding-top: 25% !important;
+  }
+  .Logo{
+    width: 60%;
+  }
 }
 </style>
