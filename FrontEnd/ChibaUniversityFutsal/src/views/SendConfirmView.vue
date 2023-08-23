ef5975c6425ecc771b1c9d749f0fd2b3748e2df6
tsyu12345 <syukut@gmail.com>
2023-08-24 00:01:00 +0900

    スマホ、PC用レイアウト調整。個別HTML構造となっていたところを統一し、再調整。
    【変更点】
    ・main.cssで#appのスタイルを指定
    ・各ルーティングページで、無理やりスマホ用とPC用でtemplateの構造を分けていたのを修正。1つのtemplateでレスポンシブに対応。
    ・その他スマホ用レイアウトでコンテンツ領域のpaddingなど余白を修正
    ・カレンダーコンポーネントを各ページに実装。

--- FrontEnd/ChibaUniversityFutsal/src/views/SendConfirmView.vue
+++ FrontEnd/ChibaUniversityFutsal/src/views/SendConfirmView.vue
@@ -1,22 +1,64 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import BaseTitle from '@/components/BaseTitle.vue';
 import KidsDetail from '../components/KidsDetail.vue';
+import BaseCalender from '../components/BaseCalender.vue';
+
+const nowMonth = ref(new Date().getMonth() + 1);
+const isMdAndUp = ref(window.matchMedia('(min-width: 768px)').matches);
+const showWeeks = ref(-1)
+
+onMounted(() => {
+    const mediaQueryList = window.matchMedia('(min-width: 768px)');
+
+    /**画面幅変更時のハンドラ*/
+    const handler = (e) => {
+        isMdAndUp.value = e.matches;
+        console.log(isMdAndUp.value);
+        showWeeks.value = isMdAndUp.value ? -1 : 2;
+    };
+    mediaQueryList.addEventListener('change', handler);
+    // 初期値を設定
+    isMdAndUp.value = mediaQueryList.matches;
+    showWeeks.value = isMdAndUp.value ? -1 : 2;
+    onUnmounted(() => {
+        mediaQueryList.removeEventListener('change', handler);
+    });
+});
+
+
 </script>
 
-
 <template>
-    <div class="row">
-        <div class="col">
-            <BaseTitle title="送信済み"></BaseTitle>
+    <div>
+        <div class="row mb-4">
+            <div class="col-12 col-md d-flex align-items-center">
+                <div class="w-fit h-fit my-auto mx-auto">
+                    <div class="w-fit mx-auto">
+                        <i class="fa-regular fa-circle-check fa-8x" style="color: #ffad1f;"></i>
+                    </div>
+                    <BaseTitle title="送信済み" class="mt-3"></BaseTitle>
+                </div>
+            </div>
+            <div class="col-12 col-md">
+                <BaseCalender :month="nowMonth" :weeks-to-show="showWeeks"/>
+            </div>
         </div>
-        <div class="col">
-            <!---カレンダーゾーン-->
-        </div>
-        <div class="col">
-            <KidsDetail/>
+        <div class="row">
+            <div class="col">
+                <div class="mx-auto" :class="isMdAndUp ? 'w-50' : 'w-100'">
+                    <KidsDetail/>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.w-fit {
+    width: fit-content;
+}
+.h-fit {
+    height: fit-content;
+}
+</style>
