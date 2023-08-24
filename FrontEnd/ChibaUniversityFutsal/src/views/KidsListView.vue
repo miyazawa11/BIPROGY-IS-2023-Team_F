2bfa18d083d6fd2d7c87f49eab04b59506ad4697
miyazawa11 <k.miyazawa.howcollect@gmail.com>
2023-08-24 20:44:20 +0900

    kidsListの修正

--- FrontEnd/ChibaUniversityFutsal/src/views/KidsListView.vue
+++ FrontEnd/ChibaUniversityFutsal/src/views/KidsListView.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+const colClass = ref('col-3');
 const kids=([
     {id:1,name:"みやざわ こうき",attend:true,confirm:false},
     {id:2,name:"みやざわ こうき",attend:true,confirm:true},
     {id:3,name:"みやざわ こうき",attend:true,confirm:false},
     {id:4,name:"みやざわ こうき",attend:true,confirm:false},
-    {id:5,name:"みやざわ こうき",attend:true,confirm:false},
+    {id:5,name:"みやざわ こうき",attend:false,confirm:false},
     {id:6,name:"みやざわ こうき",attend:true,confirm:false},
     {id:7,name:"みやざわ こうき",attend:true,confirm:true},
     {id:8,name:"みやざわ こうき",attend:true,confirm:false},
@@ -25,32 +28,30 @@ const kids=([
 </script>
 
 <template>
-    <div>
-        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-            <label class="btn btn-outline-primary" for="btnradio1">Radio 1</label>
+    <div class="mb-10">
+        <div class="btn-group" role="group" aria-label="Basic radio toggle button group" style="width:300px;">
+            <input type="radio" class="btn-check mx-auto" name="btnradio" id="btnradio1" autocomplete="off" checked>
+            <label class="btn btn-outline-primary mx-auto" for="btnradio1">ID順</label>
 
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-            <label class="btn btn-outline-primary" for="btnradio2">Radio 2</label>
+            <input type="radio" class="btn-check mx-auto" name="btnradio" id="btnradio2" autocomplete="off">
+            <label class="btn btn-outline-primary mx-auto" for="btnradio2">チェック済み</label>
 
-            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-            <label class="btn btn-outline-primary" for="btnradio3">Radio 3</label>
+            <input type="radio" class="btn-check mx-auto" name="btnradio" id="btnradio3" autocomplete="off">
+            <label class="btn btn-outline-primary mx-auto" for="btnradio3">チェック無し</label>
         </div>
-        <div class="w-100 p-3 color:">
-            <nav class="w-75 p-3 mx-auto">
-                <ul class="list-group height">
-                    <li class="list-group-item row d-flex" v-for="kid in kids" :key="kid.id">
-                        <div class="col-1  mx-auto">
-                            <input class="form-check-input" type="checkbox" v-model="kid.confirm">
+        <div class="">
+            <nav class="p-6 mx-auto">
+                <ul class="list-group overflow-auto vh-65">
+                    <li class="list-group-item row d-flex d-flex align-items-center" v-for="kid in kids" :key="kid.id">
+                        <label :class="['fs-4', colClass, 'mx-auto', 'form-check-label', 'text-center', 'd-flex', 'align-items-center']" :for="`checkbox-${kid.id}`">{{ kid.id }}</label>
+                        <div :class="['mx-auto text-center', colClass, 'name-hover']">
+                            <router-link :to="{name:'KidDetailViewPage',params:{id:kid.id}}" class="name-hover" style="color: inherit;">
+                                <button type="button" class="fs-4">{{ kid.name }}</button>
+                            </router-link>
                         </div>
-                        <label class="col-2  mx-auto form-check-label" :for="`checkbox-${kid.id}`">id:{{ kid.id }}</label>
-                        <div class="col-5  mx-auto">
-                            <routerLink :to="{name:'KidDetailViewPage',params:{id:kid.id}}">
-                                <button type="button" class="btn hover:btn-light">{{ kid.name }}</button>
-                            </routerLink>
-                        </div>
-                        <div class="col-2  mx-auto" v-if="kid.attend">〇</div>
-                        <div class="col-2  mx-auto" v-else>×</div>
+                        <span :class="['mx-auto fw-bolder fs-4 d-flex align-items-center', colClass]" style="vertical-align: middle;" v-if="kid.attend">出席</span>
+                        <span :class="['mx-auto fw-bolder fs-4 d-flex align-items-center', colClass]" style="vertical-align: middle;" v-else>欠席</span>
+                        <input :class="['form-check-input text-center d-flex align-items-center', colClass]" type="checkbox" v-model="kid.confirm">
                     </li>
                 </ul>
             </nav>
@@ -59,10 +60,19 @@ const kids=([
 </template>
 
 <style scoped>
-.height {
-    height: 100vh; 
-    width:100%;
-    overflow-x: hidden; 
-    overflow-y: auto;
+.list-group {
+    overflow: auto;
+    max-height: 65vh;
+}
+.name-hover:hover {
+    background-color: #f0f0f0; /* 任意の色 */
+  }
+.form-check-input {
+  border-color: #0000ff !important;
+  width: 40px !important;  /* 幅 */
+  height: 40px !important; /* 高さ */
+  /* 以下の行は、大きさが変更された後の配置を調整します */
+  vertical-align: middle !important;
+  margin: 0 auto !important;
 }
 </style>
